abc7b1be83b051fe4f62eb4805ddf3d6ac82408c
Samantha Gatt <smfgatt@gmail.com>
2018-07-26 09:40:50 -0400

    Initial commit

---
+++ Poll/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  Poll
+//
+//  Created by Samantha Gatt on 7/26/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+
+struct Vote {
+    let name : String
+    let response : String
+}
